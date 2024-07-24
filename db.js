600c33276ba1076981e5b8663b22d34f831cd8bc
priyansh0202-coder <priyanshsoni36@gmail.com>
2024-07-25 01:22:16 +0530

    changes

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
