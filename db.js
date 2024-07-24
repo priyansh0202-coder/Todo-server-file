af41b8564f233f80eab339964ee314639266d8fa
priyansh0202-coder <priyanshsoni36@gmail.com>
2024-07-24 23:54:16 +0530

    env file

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
-const mysql = require("mysql2")
+const mysql = require("mysql2");
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Password",
-    database: "users"
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        process.exit(1);
+    }
+    console.log('Connected to the database.');
+});
 
 module.exports = db;
